f22ca5450b98517fc058567b04001f8cfb4ff120
disservin-security-test <141495310+disservin-security-test@users.noreply.github.com>
2024-01-02 12:12:50 -0800

    Update search.cpp
    Bench: 1480561

--- src/search.cpp
+++ src/search.cpp
@@ -1011,7 +1011,7 @@ moves_loop:  // When in check, search starts here
                 {
                     Piece capturedPiece = pos.piece_on(to_sq(move));
                     int   futilityEval =
-                      ss->staticEval + 238 + 305 * lmrDepth + PieceValue[capturedPiece]
+                      ss->staticEval + 238 + 306 * lmrDepth + PieceValue[capturedPiece]
                       + captureHistory[movedPiece][to_sq(move)][type_of(capturedPiece)] / 7;
                     if (futilityEval < alpha)
                         continue;
